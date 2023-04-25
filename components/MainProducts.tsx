f572ef10ca5afba90d92759ca904cef6a8d5b020
ameenSpeakerbox <ameen466speakerbox@gmail.com>
2023-04-25 18:31:09 +0530

    modified:   components/MainProducts.tsx

--- components/MainProducts.tsx
+++ components/MainProducts.tsx
@@ -1,31 +1,32 @@
 import Image from "next/image";
 import React from "react";
 
-const MainProducts = ({
-  data,
-}: {
-  data: [{ picture: string; foodName: string; rate: number }];
-}) => {
+const MainProducts = ({ data }: any) => {
   return (
     <div className="mt-10 grid gap-5">
-      {data.map((item, idx: React.Key) => (
-        <div
-          key={idx}
-          className="flex items-center justify-center w-full border-[2px] border-brown rounded-[8px] overflow-hidden bg-[#F6ECE0] min-h-[200px]"
-        >
-          <div className="w-[30%] shrink-0 h-full overflow-hidden">
-            <Image
-              src={item.picture}
-              alt={`${item.foodName} picture`}
-              width={100}
-              height={100}
-              loading="lazy"
-              className="w-full h-full object-cover"
-            />
+      {data.map(
+        (
+          item: { picture: string; foodName: string; rate: number },
+          idx: React.Key
+        ) => (
+          <div
+            key={idx}
+            className="flex items-center justify-center w-full border-[2px] border-brown rounded-[8px] overflow-hidden bg-[#F6ECE0] min-h-[200px]"
+          >
+            <div className="w-[30%] shrink-0 h-full overflow-hidden">
+              <Image
+                src={item.picture}
+                alt={`${item.foodName} picture`}
+                width={100}
+                height={100}
+                loading="lazy"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="w-full">\</div>
           </div>
-          <div className="w-full">\</div>
-        </div>
-      ))}
+        )
+      )}
     </div>
   );
 };
